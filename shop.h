805578661c949d5e2702aecee9e8feb2b20575b7
Arif Ali <arif@fppnt.com>
2020-02-06 13:12:36 +0000

    added basic structure of classes to pass through data

--- shop.h
+++ shop.h
@@ -1,30 +1,63 @@
 #include <iostream>
+#include <memory>
 #include <map>
+#include <vector>
+#include <iostream>
+
+enum food {beans, egg, bread, milk};
+
+using orderList = std::map<food, double>;
 
 struct Product {
-std::string name;
-size_t quantity;
+    Product(food value, size_t quantity, double price) : name(value), quantity(quantity), price(price) {}
+    food name;
+    size_t quantity;
+    double price;
+};
+
+class Customer {
+    public:
+    Customer();
+    void push(orderList&);
+    void putInTrolley(std::vector<std::unique_ptr<Product>> &&);
+    //Customer(std::map<food,int>);
+
+    private:
+    ~Customer();
+    orderList m_shoppingList;
+    std::shared_ptr<Shop> m_shop;
+    std::vector<std::unique_ptr<Product>> m_trolley;
 };
 
 class Shop {
     public:
     Shop();
-    
+    std::vector<std::unique_ptr<Product>> recv(orderList shoppingList);
+    void putInCheckout (std::vector<std::unique_ptr<Product>> &&);
+    void Shop::register_customer(Customer& c);
+
     private:
     ~Shop();
-    
-    //recv();
+    void markup();
+    std::unique_ptr<Supplier> m_supplier;
+    orderList m_customerList;
+    std::vector<std::unique_ptr<Product>> m_stockList;
+    std::vector<std::unique_ptr<Product>> m_checkout;
+    std::shared_ptr<Customer> m_customer;
+
+    double markupRate = 1.25;
 };
-enum class food {beans, egg, bread, milk};
 
-class Customer {
-    Customer();
-    Customer(std::map<food,int>);
-    ~Customer();
+class Supplier{
+    public:
+    void recv(orderList& shoppingList);
+    void register_shop(Shop&);
 
-    void push();//push: shop_ptr->recv(m_shoplist)
     private:
-    std::map<food, int> m_shoplist;
-    
+    Supplier();
+    ~Supplier();
+    orderList m_priceList = {{food::beans, 0.30}, {food::egg, 0.4}, {food::bread, 0.5}, {food::milk, 0.7}};
+    std::vector<std::unique_ptr<Product>> m_checkout;
+    std::shared_ptr<Shop> m_shop;
 
 };
