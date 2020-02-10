48dad0319815dc8b4cb976655ac5d9a6db2cd4a4
Arif Ali <arif@fppnt.com>
2020-02-10 06:26:36 +0000

    simplified classes, shop does not request stock for customer orders.

--- shop.cpp
+++ shop.cpp
@@ -1,48 +1,65 @@
 #include "shop.h"
 
-Customer::Customer(Shop& s) {m_shop = std::make_shared<Shop>(&s);}
-
-void Customer::push(orderList& shoppingList){
+void Customer::shoppingList(orderList shoppingList){
     m_shoppingList = shoppingList;
-    m_shop->recv(m_shoppingList);
 };
 
-void Customer::putInTrolley(std::vector<std::unique_ptr<Product>> && products){
+Products&& Customer::doShopping(Shop& shop){
+    return shop.buy(m_shoppingList);
+};
+
+void Customer::putInTrolley(Products&& products){
     for (auto &p : products){
         m_trolley.push_back(std::move(p));
         std::cout << std::to_string(p->name) << " " << p->price << " " << p->quantity << std::endl;
     }
 };
 
-Shop::Shop(Supplier& s) {*m_supplier = s;} //std::make_unique<Shop>(&s)
-
-void Shop::recv(orderList shoppingList){
-    m_customerList = shoppingList;
-    for (auto& c : m_customerList) {
+Products&& Shop::buy(orderList shoppingList){
+    for (auto& c : shoppingList) {
         size_t count(0); //reset count for each food
         for (auto& s : m_stockList) {//iterate shop's current stock
-            if (count < c.second){
-                if (c.first == s->name){
+            if (count < 1){
+                if (s != nullptr && c.first == s->name){
+                    s->quantity = c.second; //quantity odered by customer
+                    //double price = s->price;
+                    s->price = s->price * c.second * markupRate; // price for quantity
                     m_checkout.push_back(std::move(s)); //move product from stock to checkout
                     ++count;
                     //continue;
                 }
             } else{ break;}
+        }//if (count < 1){//if product not in shop stock, send order to supplier
+        
+        //    orderList m ={{c.first,(c.second-count)}};
+        //    m_supplier->recv(m);
+        //}
+    }
+    //markup();
+    //m_customer->putInTrolley(std::move(m_checkout));//move checkout products into customer's trolley
+    return std::move(m_checkout);
+};
 
-        }if (count < c.second){ //if product not in shop stock, send order to supplier
-            orderList m ={{c.first,(c.second-count)}};
-            m_supplier->recv(m);
+void Shop::supplyList(orderList supplyList){
+    m_supplyList = supplyList;
+};
+
+void Shop::buySupplies(Supplier& sup){
+    m_stockList = sup.buy(m_supplyList);
+}
+void Shop::printstock(){
+    std::cout << "\ncurrent stock:" << std::endl;
+    for (auto &p : m_stockList){
+        if(p != nullptr){
+            std::cout << "item: " << p->name  << std::endl;}
         }
-    }
-    markup();
-    m_customer->putInTrolley(std::move(m_checkout));//move checkout products into customer's trolley
 };
 
-void Shop::putInCheckout(std::vector<std::unique_ptr<Product>>&& products){
-    for (auto& p : products){
-        m_checkout.push_back(std::move(p));
-    }
-};
+// void Shop::putInCheckout(Products&& products){
+//     for (auto& p : products){
+//         m_checkout.push_back(std::move(p));
+//     }
+// };
 
 void Shop::markup(){
     for (auto& p : m_checkout){
@@ -51,19 +68,20 @@ void Shop::markup(){
     }
 };
 
-void Shop::register_customer(Customer& c){
-    m_customer = std::make_unique<Customer>(&c);
-};
+// void Shop::register_customer(Customer& c){
+//     m_customer = std::make_unique<Customer>(&c);
+// };
 
-void Supplier::register_shop(Shop& s){
-    m_shop = std::make_shared<Shop>(&s);
-};
+// void Supplier::register_shop(Shop& s){
+//     m_shop = std::make_shared<Shop>(&s);
+// };
 
-void Supplier::recv(orderList& shoppingList){
-    for (auto& c : shoppingList) {
-        for ( int i = 0; i < c.second; ++i){
+Products&& Supplier::buy(orderList shoppingList){
+    for (auto& c : shoppingList){
+        //for ( int i = 0; i < c.second; ++i){
             m_checkout.push_back(std::unique_ptr<Product>(new Product(c.first, 1, m_priceList[c.first])));
-        }
-        m_shop->putInCheckout(std::move(m_checkout));//put new products into shop checkout
+        //}
+        //m_shop->putInCheckout(std::move(m_checkout));//put new products into shop checkout
     }
+    return std::move(m_checkout);
 };
--- shop.h
+++ shop.h
@@ -2,61 +2,69 @@
 #include <memory>
 #include <map>
 #include <vector>
-#include <iostream>
 
-enum food {beans, egg, bread, milk, beans_6pk, egg_12pk};
+enum food {beans, egg, bread, milk, bananas};
 
 using orderList = std::map<food, double>;
 
-class Supplier{
-    public:
-    Supplier();
-    ~Supplier();
-    void recv(orderList& shoppingList);
-    void register_shop(Shop& s);
-
-    private:
-    orderList m_priceList = {{food::beans, 0.30}, {food::egg, 0.1}, {food::bread, 0.5}, {food::milk, 0.7}, {food::beans_6pk, 1.5}, {food::egg_12pk, 1.2}};
-    std::vector<std::unique_ptr<Product>> m_checkout;
-    std::shared_ptr<Shop> m_shop;
-
-};
 struct Product {
     Product(food value, size_t quantity, double price) : name(value), quantity(quantity), price(price) {}
     food name;
-    size_t quantity;
+    double quantity;
     double price;
+};
+
+using Products = std::vector<std::unique_ptr<Product>>;
+
+class Supplier{
+    public:
+    // Supplier();
+    // ~Supplier();
+    //void recv(orderList& shoppingList);
+    Products&& buy(orderList shoppingList);
+    //void register_shop(Shop& s);
+
+    private:
+    orderList m_priceList = {{food::beans, 0.30}, {food::egg, 0.1}, {food::bread, 0.5}, {food::milk, 0.7}, {bananas, 1}};
+    Products m_checkout;
+    //std::shared_ptr<Shop> m_shop;
+
 };
 
 class Shop {
     public:
-    Shop(Supplier& sup);
-    ~Shop();
-    void recv(orderList shoppingList);
-    void putInCheckout (std::vector<std::unique_ptr<Product>> &&);
-    void Shop::register_customer(Customer& c);
+    // Shop();
+    // ~Shop();
+    Products&& buy(orderList shoppingList);
+    void supplyList(orderList);
+    void buySupplies(Supplier& s);
+    void printstock();
+    //void putInCheckout (Products &&);
+    //void Shop::register_customer(Customer& c);
 
     private:
     void markup();
     std::unique_ptr<Supplier> m_supplier;
     orderList m_customerList;
-    std::vector<std::unique_ptr<Product>> m_stockList;
-    std::vector<std::unique_ptr<Product>> m_checkout;
-    std::unique_ptr<Customer> m_customer;
+    orderList m_supplyList;
+    Products m_stockList;
+    Products m_checkout;
+    //std::unique_ptr<Customer> m_customer;
 
     double markupRate = 1.25;
 };
 
 class Customer {
     public:
-    Customer(Shop& s);
-    ~Customer();
-    void push(orderList&);
-    void putInTrolley(std::vector<std::unique_ptr<Product>> &&);
+    // Customer();
+    // ~Customer();
+    Products&& doShopping(Shop& s);
+    void shoppingList(orderList);
+    void putInTrolley(Products&&);
     //Customer(std::map<food,int>);
 
     private:
     orderList m_shoppingList;
     std::shared_ptr<Shop> m_shop;
-    std::vector<std::unique_ptr<Product>> m_trolley;
+    Products m_trolley;
 };
